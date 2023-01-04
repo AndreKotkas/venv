773236c7cd1240eeeddf424292f65f38e383ec1d
User <user@SDAcourse.pl>
2023-01-04 23:57:41 +0200

    kas see töötab

--- finalproject/urls.py
+++ finalproject/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from map import views as map_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", map_views.index, name='index'),
 ]
---
+++ map/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+import folium
+
+# Create your views here.
+def index(request):
+    #Create map object
+    m = folium.Map(location=[59, 25], zoom_start=8)
+    folium.Marker([58.378485, 26.730052]).add_to(m)
+    #Get HTML Representation of Map Objects
+    m = m._repr_html_()
+    context ={
+        'm': m,
+    }
+    return render(request, 'index.html', context)
