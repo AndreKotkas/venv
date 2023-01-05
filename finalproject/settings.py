7bff45acc57273db6ceb6032cf9b79415f2b3d6c
User <user@SDAcourse.pl>
2023-01-05 12:47:25 +0200

    test on map

--- finalproject/settings.py
+++ finalproject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "map.apps.MapConfig",
+    "crispy_forms",
 ]
 
 MIDDLEWARE = [
@@ -100,6 +101,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
--- map/views.py
+++ map/views.py
@@ -1,15 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 import folium
 import geocoder
 from .models import Search
+from .forms import SearchForm
 
 # Create your views here.
 def index(request):
+    if request.method == 'POST':
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    else:
+        form = SearchForm()
     address = Search.objects.all().last()
     location = geocoder.osm(address)
     lat = location.lat
     lng = location.lng
     country = location.country
+    if lat == None or lng == None:
+        address.delete()
+        return HttpResponse('Address you inbut is unvalid')
     #Create map object
     m = folium.Map(location=[59, 25], zoom_start=8)
     #folium.Marker([58.378485, 26.730052], tooltip='Click for more', popup='Sõbralt Sõbrale').add_to(m)
@@ -19,5 +32,6 @@ def index(request):
     m = m._repr_html_()
     context ={
         'm': m,
+        'form': form,
     }
     return render(request, 'index.html', context)
