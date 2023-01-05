8f8c26fc462398736f40f405eb05bc388da80de6
Karoliine Pensa <karoliinepensa@gmail.com>
2023-01-05 19:19:44 +0200

    test

--- finalproject/urls.py
+++ finalproject/urls.py
@@ -13,11 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from map import views as map_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", map_views.index, name='index'),
+    path('', include('blog.urls'))
 ]
+
