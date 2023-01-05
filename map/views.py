eed800be969254337bf659bb11c1b712c2132a15
User <user@SDAcourse.pl>
2023-01-05 10:27:19 +0200

    test on marker shows info

--- map/views.py
+++ map/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 import folium
+import geocoder
 
 # Create your views here.
 def index(request):
+    location = geocoder.osm('EE')
+    lat = location.lat
+    lng = location.lng
+    country = location.country
     #Create map object
     m = folium.Map(location=[59, 25], zoom_start=8)
     folium.Marker([58.378485, 26.730052], tooltip='Click for more', popup='Sõbralt Sõbrale').add_to(m)
+    folium.Marker([lat, lng], tooltip='Click for more', popup=country).add_to(m)
     #Get HTML Representation of Map Objects
     m = m._repr_html_()
     context ={
