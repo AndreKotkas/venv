71934803783be26365ac2a8d3f8af677597446d7
User <user@SDAcourse.pl>
2023-01-05 11:12:59 +0200

    test on map

--- map/views.py
+++ map/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render
 import folium
 import geocoder
+from .models import Search
 
 # Create your views here.
 def index(request):
-    #address = request.POST.get('address')
-    location = geocoder.osm('EE')
+    address = Search.objects.all().last()
+    location = geocoder.osm(address)
     lat = location.lat
     lng = location.lng
     country = location.country
     #Create map object
     m = folium.Map(location=[59, 25], zoom_start=8)
-    folium.Marker([58.378485, 26.730052], tooltip='Click for more', popup='Sõbralt Sõbrale').add_to(m)
+    #folium.Marker([58.378485, 26.730052], tooltip='Click for more', popup='Sõbralt Sõbrale').add_to(m)
+
     folium.Marker([lat, lng], tooltip='Click for more', popup=country).add_to(m)
     #Get HTML Representation of Map Objects
     m = m._repr_html_()
