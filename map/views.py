1f29e8a909b2df33157b3ee3f394b8c5dbb562f5
User <user@SDAcourse.pl>
2023-01-13 19:14:25 +0200

    test on map

--- map/views.py
+++ map/views.py
@@ -6,46 +6,54 @@ import branca
 import pandas as pd
 from .models import Search
 from .forms import SearchForm
+import csv
 
 
 # Create your views here.
-def index(request):
-    if request.method == 'POST':
+def index(request) :
+    if request.method == 'POST' :
         form = SearchForm(request.POST)
-        if form.is_valid():
+        if form.is_valid() :
             form.save()
             return redirect('/')
 
-    else:
+    else :
         form = SearchForm()
     address = Search.objects.all().last()
     location = geocoder.osm(address)
     lat = location.lat
     lng = location.lng
     country = location.country
-    if lat == None or lng == None:
+    if lat == None or lng == None :
         address.delete()
-        return HttpResponse('Address you inbut is unvalid')
-    #Create map object
+        return HttpResponse('Address you input is invalid')
+    # Create map object
+
+    franchies = pd.read_csv("ThriftStores.csv")
+    franchies.head()
     m = folium.Map(location=[59, 25], zoom_start=8)
-    data = pd.DataFrame({
-        'lon' : [26.765688132398214, 24.735112709860474, 25.59881480043152],
-        'lat' : [58.36940994869108, 59.441412800684276, 58.36333595519406],
-        'name' : ['Sõbralt Sõbrale Annelinn', 'Sõbralt Sõbrale Balti Jaama Turg', 'Sõbralt Sõbrale Viljandi'],
-    }, dtype=str)
+    for index, franchies in franchies.iterrows():
+        location = [franchies['LAT'], franchies['LONG']]
+        folium.Marker(location, popup=f'{franchies["CITY"], franchies["STORE_NAME"], franchies["ADDRESS"], franchies["OPEN_TIME"], franchies["PHONE_NR"]}').add_to(m)
 
-    for i in range(0, len(data)) :
-        folium.Marker(
-            location=[data.iloc[i]['lat'], data.iloc[i]['lon']],
-            popup=data.iloc[i]['name'],
-        ).add_to(m)
+    # m.save("ThriftStores.csv")
+    # data = pd.DataFrame({
+    #     'lon' : [26.765688132398214, 24.735112709860474, 25.59881480043152],
+    #     'lat' : [58.36940994869108, 59.441412800684276, 58.36333595519406],
+    #     'name' : ['Sõbralt Sõbrale Annelinn', 'Sõbralt Sõbrale Balti Jaama Turg', 'Sõbralt Sõbrale Viljandi'],
+    # }, dtype=str)
 
-    # folium.Marker([lat, lng], tooltip='Click for more', popup=country).add_to(m)
-    #Get HTML Representation of Map Objects
+    # for i in range(0, len(data)) :
+    #     folium.Marker(
+    #         location=[data.iloc[i]['lat'], data.iloc[i]['lon']],
+    #         popup=data.iloc[i]['name'],
+    #     ).add_to(m)
+    #
+    # # folium.Marker([lat, lng], tooltip='Click for more', popup=country).add_to(m)
+    # #Get HTML Representation of Map Objects
     m = m._repr_html_()
-    context ={
-        'm': m,
-        'form': form,
+    context = {
+        'm' : m,
+        'form' : form,
     }
     return render(request, 'index.html', context)
-
