76af4efa225c761ef59f1e10e6efaa4b00ffc0fc
User <user@SDAcourse.pl>
2023-01-22 14:23:27 +0200

    test on map

--- map/views.py
+++ map/views.py
@@ -36,20 +36,7 @@ def index(request) :
         location = [franchies['LAT'], franchies['LONG']]
         folium.Marker(location, popup=f'{franchies["CITY"], franchies["STORE_NAME"], franchies["ADDRESS"], franchies["OPEN_TIME"], franchies["PHONE_NR"]}').add_to(m)
 
-    # m.save("ThriftStores.csv")
-    # data = pd.DataFrame({
-    #     'lon' : [26.765688132398214, 24.735112709860474, 25.59881480043152],
-    #     'lat' : [58.36940994869108, 59.441412800684276, 58.36333595519406],
-    #     'name' : ['Sõbralt Sõbrale Annelinn', 'Sõbralt Sõbrale Balti Jaama Turg', 'Sõbralt Sõbrale Viljandi'],
-    # }, dtype=str)
 
-    # for i in range(0, len(data)) :
-    #     folium.Marker(
-    #         location=[data.iloc[i]['lat'], data.iloc[i]['lon']],
-    #         popup=data.iloc[i]['name'],
-    #     ).add_to(m)
-    #
-    # # folium.Marker([lat, lng], tooltip='Click for more', popup=country).add_to(m)
     # #Get HTML Representation of Map Objects
     m = m._repr_html_()
     context = {
