81bb36451aee36db22db8113e981f53296f63e86
User <user@SDAcourse.pl>
2023-02-10 12:33:18 +0200

    test on map

--- map/forms.py
+++ map/forms.py
@@ -1,10 +1,1 @@
 from django import forms
-from .models import Search
-
-class SearchForm(forms.ModelForm):
-    address = forms.CharField(label='')
-
-    class Meta:
-        model = Search
-        fields = ['address']
-
